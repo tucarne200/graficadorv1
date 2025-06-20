bef46795479e0faddf55196d81f48c228fed15e3
tucarne200 <tucarne200@gmail.com>
2025-06-20 16:04:38 -0600

    asd

--- interfazprueba.py
+++ interfazprueba.py
@@ -31,34 +31,7 @@ class AnalisisDatosGUI:
 
         # Agrega aquí el código para los gráficos según tus necesidades.
         df['Agente'].value_counts().plot(kind='bar', ax=axes[0, 0], color='skyblue')
-        axes[0, 0].set_title('Carga de Trabajo por Agente')
-
-        df['Ejecutivo pre asignado'].value_counts().plot(kind='bar', ax=axes[0, 1], color='salmon')
-        axes[0, 1].set_title('Carga de Trabajo por Ejecutivo Pre Asignado')
-
-        df['Ciudad'].value_counts().head(10).plot(kind='bar', ax=axes[1, 0], color='lightgreen')
-        axes[1, 0].set_title('Ciudades más Repetidas')
-
-        df['Estado'].value_counts().head(10).plot(kind='bar', ax=axes[1, 1], color='lightcoral')
-        axes[1, 1].set_title('Estados más Repetidos')
-
-        cliente_relacion = df['Cliente'].value_counts()
-        cliente_relacion.plot(kind='pie', autopct='%1.1f%%', colors=['gold', 'lightcoral'], ax=axes[2, 0])
-        axes[2, 0].set_title('Relación Cliente/No Cliente')
-
-        giro_relacion = df['Giro'].apply(lambda x: 'Lleno' if pd.notna(x) else 'Vacío').value_counts()
-        giro_relacion.plot(kind='pie', autopct='%1.1f%%', colors=['lightblue', 'lightpink'], ax=axes[2, 1])
-        axes[2, 1].set_title('Relación Giro Lleno/Vacío')
-
-        plt.tight_layout()
-
-        # Muestra una ventana con los gráficos
-        self.mostrar_ventana_graficos()
-
-    def mostrar_ventana_graficos(self):
-        plt.show()
-
-if __name__ == "__main__":
+        ax
     root = Tk()
     gui = AnalisisDatosGUI(root)
     root.mainloop()
