4ba3062dbd32cdbe95364135f6678746e9e5df4a
tucarne200 <tucarne200@gmail.com>
2025-06-20 16:00:26 -0600

    correccion de errores

--- interfazprueba.py
+++ interfazprueba.py
@@ -8,18 +8,7 @@ class AnalisisDatosGUI:
         self.root = root
         self.root.title("Análisis de Datos")
 
-        self.label = Label(root, text="Arrastra y suelta un archivo Excel aquí:")
-        self.label.pack(pady=20)
 
-        self.button = Button(root, text="Seleccionar Archivo", command=self.cargar_archivo)
-        self.button.pack(pady=20)
-
-    def cargar_archivo(self):
-        archivo_path = filedialog.askopenfilename(filetypes=[("Archivos Excel", "*.xlsx;*.xls")])
-        if archivo_path:
-            self.realizar_analisis(archivo_path)
-
-    def realizar_analisis(self, archivo_path):
         df = pd.read_excel(archivo_path)
 
         # Realizar análisis de datos y generar gráficos
